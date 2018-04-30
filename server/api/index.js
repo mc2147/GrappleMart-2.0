3637e3357e4b589880d87368e39d2c03f3ffa677
mc2147 <matthewchan2147@gmail.com>
2018-04-29 20:20:49 -0500

    m

--- server/api/index.js
+++ server/api/index.js
@@ -12,17 +12,17 @@ router.use('/products', require('./products'))
 
 router.get('/country-seeds', (req, res) => {
   let countrySeeds = {};
-  // countryCodes['countryCodes'].forEach(elem => {
-  //   let randomNum = Math.floor(200*Math.random());
-  //   countrySeeds[elem.code] = {purchases: randomNum}    
-  // })
-  for (var K in countryCodes['countryCodes']) {
+  countryCodes['countryCodes'].forEach(elem => {
     let randomNum = Math.floor(200*Math.random());
-    let obj = countryCodes['countryCodes'][K];
-    obj.purchases = Math.floor(200*Math.random());
-    // countrySeeds[K]
-    countrySeeds[K] = {purchases: randomNum, code: K}
-  }
+    countrySeeds[elem.code] = {purchases: randomNum, code: elem.code}    
+  })
+  // for (var K in countryCodes['countryCodes']) {
+  //   let randomNum = Math.floor(200*Math.random());
+  //   let obj = countryCodes['countryCodes'][K];
+  //   obj.purchases = Math.floor(200*Math.random());
+  //   // countrySeeds[K]
+  //   countrySeeds[K] = {purchases: randomNum, code: K}
+  // }
   res.send(JSON.stringify(countrySeeds));
 })
 
